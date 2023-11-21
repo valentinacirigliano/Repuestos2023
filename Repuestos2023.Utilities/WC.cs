a52fbd50cff105ef17c41757783f8fc797559436
Valentina Cirigliano <valentinacirigliano@hotmail.com>
2023-11-20 21:04:02 -0300

    Agregar archivos de proyecto.

---
+++ Repuestos2023.Utilities/WC.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repuestos2023.Utilities
+{
+    public static  class WC
+    {
+        public const string Role_User_Individual = "Individual";
+        public const string Role_User_Company = "Company";
+        public const string Role_Admin = "Admin";
+        public const string Role_Customer = "Customer";
+    }
+}
---
+++ Repuestos2023MVC.Web/wwwroot/js/repuesto.js
@@ -0,0 +1,56 @@
+﻿$(document).ready(function () {
+
+    var repuestoTable = $('#tblRepuestos').DataTable({
+        "ajax": {
+            "url": "/Repuestos/GetAll"  
+        },
+        "columns": [
+            { "data": "descripcion" },
+            { "data": "precioLista" },
+            { "data": "categoria.NombreCategoria" }, 
+            {
+                "data": "repuestoId",
+                "render": function (data) {
+                    return `
+                        <a class="btn btn-warning" href="/ControllerName/UpSert?id=${data}" >
+                            <i class="bi bi-pencil-square"></i>&nbsp;
+                            Editar
+                        </a>
+                        <a class="btn btn-danger" onclick="Eliminar('/ControllerName/Eliminar/${data}')" >
+                            <i class="bi bi-trash3"></i> &nbsp;
+                            Eliminar
+                        </a>
+                    `;
+                }
+            }
+        ]
+    });
+
+    function Eliminar(url) {
+        Swal.fire({
+            title: '¿Estás seguro?',
+            text: "¡No podrás revertir esto!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sí, eliminarlo'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    "url": url,
+                    "type": 'DELETE',
+                    "success": function (data) {
+                        console.log(data);
+                        if (data.success) {
+                            repuestoTable.ajax.reload();
+                            toastr.success(data.message);
+                        } else {
+                            toastr.error(data.message);
+                        }
+                    }
+                });
+            }
+        });
+    }
+});
